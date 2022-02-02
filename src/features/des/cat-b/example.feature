9e03da519e71cd9ed08453e7f5fd5a2e089383ce
cai <cai.turner@dvsa.gov.uk>
2022-02-02 09:36:16 +0000

    add start of tests

--- src/features/des/cat-b/example.feature
+++ src/features/des/cat-b/example.feature
@@ -1,3 +1,6 @@
 Feature: Example feature
 
     Scenario:  Example Empty Scenario
+      Given I am signed in as a random "Super" user
+      And I wait for the loading spinner with the text "Refreshing databases"
+      And I pause for 1000ms
--- src/steps/des/when.ts
+++ src/steps/des/when.ts
@@ -1,7 +1,13 @@
 import { When } from '@cucumber/cucumber';
 import desLogin from '../../app-actions/des-action/desLogin';
+import loginWebAsUser from '../../app-actions/des-action/desLoginAsUser'
 
 When(
     /^I am signed in as a random user$/,
     desLogin
+);
+
+When(
+  /^I am signed in as a random "([^"]*)?" user$/,
+  loginWebAsUser
 );
