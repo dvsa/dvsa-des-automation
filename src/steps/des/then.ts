4b3bf998961b8c9617afcc75fab12320662b4224
Ditmir Husani <ditmir.husani@dvsa.gov.uk>
2022-02-22 21:17:40 +0000

    setup for des test center journal

--- src/steps/des/then.ts
+++ src/steps/des/then.ts
@@ -2,6 +2,6 @@ import { Then } from '@cucumber/cucumber';
 import checkCandidateTestDetailsCard from '../../app-actions/des-action/checkCandidateTestSlot';
 
 Then(
-    /^the candidates test details are shown for tommorow$/,
+    /^the candidates test details are shown$/,
     checkCandidateTestDetailsCard
 );
