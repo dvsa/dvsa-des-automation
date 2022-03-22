d14bebd5986f468a1acd364ec223b110dfff833d
Gabriel Ng <gabriel.ng@dvsa.gov.uk>
2022-03-22 19:49:54 +0000

    Refactor test journal tests

--- config/wdio.conf.ts
+++ config/wdio.conf.ts
@@ -188,6 +188,7 @@ export const config: WebdriverIO.Config = {
     profile: [], // <string[]> (name) specify the profile to use
     require: [
       './shared/boilerplate/steps/**/*.ts',
+      './shared/custom/steps/**/*.ts',
       './src/steps/ionic/*.ts',
       // Or search a (sub)folder for JS files with a wildcard
       // works since version 1.1 of the wdio-cucumber-framework
