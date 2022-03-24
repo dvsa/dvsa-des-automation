e9e855cd1875c1d6ee4f0f10d2394f71fc599e9f
cai <cai.turner@dvsa.gov.uk>
2022-03-24 14:57:02 +0000

    starting successful test cat b

--- src/steps/des/when.ts
+++ src/steps/des/when.ts
@@ -1,6 +1,9 @@
 import { When } from '@cucumber/cucumber';
 import desLogin from '../../app-actions/des-action/desLogin';
-import loginWebAsUser from '../../app-actions/des-action/desLoginAsUser';
+import loginWebAsUser from '../../app-actions/des-action/desLoginAsUser'
+import completeDeclarationPage from '../../app-actions/des-action/completeDeclarationPage';
+import completeComunicationPage from '../../app-actions/des-action/completeComunicationPage';
+import completeWaitingRoomToCarPage from '../../app-actions/des-action/completeWaitingRoomToCarPage';
 
 When(
   /^I am signed in as a random user$/,
@@ -10,4 +13,19 @@ When(
 When(
   /^I am signed in as a random "([^"]*)?" user$/,
   loginWebAsUser,
+);
+
+When(
+  /^I complete the waiting room declaration page$/,
+  completeDeclarationPage,
+);
+
+When(
+  /^I complete the waiting room communication page$/,
+  completeComunicationPage,
+);
+
+When(
+  /^I complete the waiting room to car page$/,
+  completeWaitingRoomToCarPage,
 );
