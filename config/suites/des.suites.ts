3f1d06128ea9bd9a64f443c24801772520fc8a8c
solomon <solomon.simukoko@dvsa.gov.uk>
2022-04-08 09:41:41 +0100

    MES-7381 add to suite

--- config/suites/des.suites.ts
+++ config/suites/des.suites.ts
@@ -2,5 +2,6 @@ export const DESSuites = {
   smalltest: [
     './src/features/des/dashboard-test.feature',
     './src/features/des/cat-b/SuccessfulTest.feature',
+    './src/features/des/dashboard-checks.feature',
   ],
 };
