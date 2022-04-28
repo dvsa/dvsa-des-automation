9e9074e65aca83102f8c4ebd7adcdc349108be22
Cai Jones <cai.jones@dvsa.gov.uk>
2022-04-28 10:16:49 +0100

    changed unsuccessful finalise outcome step

--- src/app-actions/des-action/completeNonPassedFinalOutcomePage.ts
+++ src/app-actions/des-action/completeNonPassedFinalOutcomePage.ts
@@ -1,8 +1,8 @@
 import DataTable from '@cucumber/cucumber/lib/models/data_table';
 import FinalOutcomePageObject from '../../pageobjects/des/finalOutcomeScreen.pageobject';
 
-export default async (table: DataTable): Promise<void> => {
+export default async (testOutcome:string, table: DataTable): Promise<void> => {
   const data = table.rowsHash();
 
-  await FinalOutcomePageObject.completeNonPassedFinalOutcomePage(data);
+  await FinalOutcomePageObject.completeNonPassedFinalOutcomePage(testOutcome, data);
 };
