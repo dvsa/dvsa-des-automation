b046da277c59afbd5cde1aea968a70ff9ca8f097
solomon <solomon.simukoko@dvsa.gov.uk>
2022-05-12 08:51:42 +0100

    MES-7816 add to DES suite

--- config/suites/des.suites.ts
+++ config/suites/des.suites.ts
@@ -5,6 +5,7 @@ export const DESSuites = {
     './src/features/des/cat-b/SuccessfulTests/SuccessfulTestWith15Faults.feature',
     './src/features/des/testCentre/testcentreJournalToday.feature',
     './src/features/des/testCentre/testcentreJournalTomorrow.feature',
+    './src/features/des/testCentre/testcentreExaminerAccount.feature',
     './src/features/des/cat-b/UnsuccessfulTests/UnsuccessfulTestWith16Faults.feature',
     './src/features/des/cat-b/UnsuccessfulTests/UnsuccessfulTestWithSeriousFaults.feature',
     './src/features/des/cat-b/UnsuccessfulTests/UnsuccessfulTestWithEyesight.feature',
