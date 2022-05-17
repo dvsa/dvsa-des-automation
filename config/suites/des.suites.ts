300995e96f58beef7babce20ff67a8728a74754f
Gabriel Ng <gabriel.ng@dvsa.gov.uk>
2022-05-17 13:57:59 +0100

    Ammend suite

--- config/suites/des.suites.ts
+++ config/suites/des.suites.ts
@@ -18,7 +18,6 @@ export const DESSuites = {
     './src/features/des/cat-b/TerminatedTests/TerminatedTestsByReturnToTestReport.feature',
     './src/features/des/cat-b/TerminatedTests/TerminatedTestsWaitingRoomToCar.feature',
     '.src/features/des/cat-b/TerminatedTests/TerminatedTestsRekey.feature',
-    '.src/features/des/cat-b/TerminatedTests/TerminatedTestsRekey.feature',
   ],
   successfultests: [
     './src/features/des/cat-b/SuccessfulTests/SuccessfulTestWith0Faults.feature',
