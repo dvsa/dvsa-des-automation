d545dbf5ce7b79bf63bc646e8b581c42a59906c3
Matthew Bell <mbell.dvsa@icloud.com>
2023-03-07 16:54:54 +0000

    Bump app dev common repo; Update copy-shared.sh script

--- copy-shared.sh
+++ copy-shared.sh
@@ -1,5 +1,3 @@
 rm -rf ./shared
 mkdir ./shared
 cp -R ./node_modules/@dvsa/app-dev-automation/src/ ./shared
-rm -rf ./creds/credentials.json
-cp ./node_modules/@dvsa/app-dev-automation/src/credentials/des/credentials.json ./creds
