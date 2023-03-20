f8db6a75093a15bf554f63dc8884016abb6f9a29
Matthew Bell <mbell.dvsa@icloud.com>
2023-03-20 10:35:11 +0000

    Appium config updates

--- config/appium.base.ts
+++ config/appium.base.ts
@@ -1,5 +1,22 @@
+// export const appiumbase = {
+//   maxInstances: 1,
+//   usePrebuiltWDA: false,
+//   autoLaunch: true,
+//   wdaLocalPort: 8200,
+//   fullContextList: true,
+//   showXcodeLog: true,
+//   autoWebview: true,
+//   useNewWDA: true, // Set this to true to reinstall the driver everytime
+//   automationName: 'XCUITest',
+//   autoAcceptAlerts: false,
+// };
+
+// https://appium.io/docs/en/writing-running-appium/caps/
+// https://github.com/appium/appium-xcuitest-driver/blob/master/docs/capabilities.md
 export const appiumbase = {
   maxInstances: 1,
+  fullReset: true, // Change this to true to reinstall the application
+  noReset: false, // noReset just clears the app data, such as its cache.
   usePrebuiltWDA: false,
   autoLaunch: true,
   wdaLocalPort: 8200,
@@ -9,4 +26,6 @@ export const appiumbase = {
   useNewWDA: true, // Set this to true to reinstall the driver everytime
   automationName: 'XCUITest',
   autoAcceptAlerts: false,
+  // enable to run without simulator UI
+  // isHeadless: true,
 };
