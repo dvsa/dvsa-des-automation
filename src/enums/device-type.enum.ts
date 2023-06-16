d3df7e454f1fe84e95f44407153b6cc7bb7d92fc
Cai Jones <cai.jones@dvsa.gov.uk>
2023-06-16 15:47:14 +0100

    rebased and updated the config

--- src/enums/device-type.enum.ts
+++ src/enums/device-type.enum.ts
@@ -5,3 +5,10 @@ export enum DeviceName {
     iPad9thGen = 'iPad (9th generation)',
     iPadPro10Point5Inch = 'iPad Pro (10.5-inch)',
 }
+
+export enum IOSVersion {
+    sixteenOne = '16.1',
+    // sixteenTwo = '16.2',
+    // sixteenThree = '16.3',
+    // sixteenFour = '16.4',
+}
