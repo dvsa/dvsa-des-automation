6605e30276b33f4095f26a5d7b3f80b55f38635f
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2023-06-21 16:51:51 +0100

    Parralel simulator config (#116)
    * Add auto generated capabiity exlusion list
    
    * Distinct features when duplicaed across suites
    
    * Add enum for devices
    
    * Scale buildConfig.maxInstances by number of capabilities
    
    * Add built config
    
    * Reverse filter to select everything except current index
    
    * ceil chunk value to ensure odd number of features scales properly
    
    * Update description of fitler
    
    * Remove http client; Add iPad air into capabilites array
    
    * rebased and updated the config
    
    * bumped sim count to 5
    
    * put sim amounts to 4
    
    * Remove http client; Add iPad air into capabilites array
    
    * updated package lock
    
    * uncommented version numbers
    
    * Mes 8757 - cat b validation  and other tests fixes (#121)
    
    * added feature file for validation
    
    * things added
    
    * create cat b validation feature with fixes across other tests as well
    
    * moved import order
    
    ---------
    
    Co-authored-by: Cai Jones <cai.jones@dvsa.gov.uk>
    
    * Remove http client; Add iPad air into capabilites array
    
    ---------
    
    Co-authored-by: Cai Jones <cai.jones@dvsa.gov.uk>
    Co-authored-by: Cai Jones <60773915+turnerc1@users.noreply.github.com>

---
+++ src/enums/device-type.enum.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export enum DeviceName {
+    iPadAir3rdGen = 'iPad Air (3rd generation)',
+    iPad8thGen = 'iPad (8th generation)',
+    iPad9thGen = 'iPad (9th generation)',
+    iPadPro10Point5Inch = 'iPad Pro (10.5-inch)',
+}
+
+export enum IOSVersion {
+    sixteenOne = '16.1',
+    sixteenTwo = '16.2',
+    sixteenThree = '16.3',
+    sixteenFour = '16.4',
+}
